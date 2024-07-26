eec71b243612e7b0f73ab9ae4d65b1e58c8ae917
shivendra <shivendrakeshari581@gmail.com>
2024-07-26 16:38:20 +0530

    fix

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,13 +1,13 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
-import Section from "./Section";
+// import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
-    <Section id="features">
+    <div id="features">
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
@@ -64,7 +64,7 @@ const Benefits = () => {
           ))}
         </div>
       </div>
-    </Section>
+    </div>
   );
 };
 
