8a9ac78f9f13bb22cbe573550c3be6d861d90735
shivendra <shivendrakeshari581@gmail.com>
2024-07-28 13:35:00 +0530

    made changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {auth ,db} from "./Firebase";
+import {auth,db} from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
 
 const Profile = () => {
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,26 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCULfD1EZ6wdE90dqweUm1WSlFU9djwF48",
-  authDomain: "login-auth-db0ae.firebaseapp.com",
-  projectId: "login-auth-db0ae",
-  storageBucket: "login-auth-db0ae.appspot.com",
-  messagingSenderId: "360585863459",
-  appId: "1:360585863459:web:e6ae99b83deff395d32c96",
-  measurementId: "G-ZZMLPV939C"
+  apiKey: "AIzaSyA36qfFJkLYd4hsxL-rrL1h12K8rQt80pE",
+  authDomain: "codev-8e7b7.firebaseapp.com",
+  projectId: "codev-8e7b7",
+  storageBucket: "codev-8e7b7.appspot.com",
+  messagingSenderId: "196878845373",
+  appId: "1:196878845373:web:3c4826be4396e61f2e7ed4",
+  measurementId: "G-MZ2T43TLDN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const auth = getAuth();
+
+export const auth=getAuth();
 export const db=getFirestore(app);
 export default app;
